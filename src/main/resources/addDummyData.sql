2abc8afb6b992eebcee0aed902fe881a687359be
Macy M <mei.mcanally@ttu.edu>
2020-12-12 20:57:44 -0600

    fix dummy data

--- src/main/resources/addDummyData.sql
+++ src/main/resources/addDummyData.sql
@@ -1,7 +1,7 @@
 INSERT INTO users
 (user_name, user_password, user_admin) VALUES
-('vincent', 'password', true),
-('william', 'password', false);
+('vincent', '$2a$12$3btNOlosyVqjblWIfZxOMeUfRsB5k8kYT.lT/OInDFWjD8b2/a.E2', true),
+('william', '$2a$12$3btNOlosyVqjblWIfZxOMeUfRsB5k8kYT.lT/OInDFWjD8b2/a.E2', false);
 
 INSERT INTO card_templates
 (user_id, card_template_description, card_front_template, card_back_template) VALUES
